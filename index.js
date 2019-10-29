bc47748c0ae84987de2e3aebc0d8f219e6bd5afd
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-29 20:48:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class Formatter {
   //add static methods here
      static capitalize (name) {
-        name.charAt(0).toUpperCase() + name.slice(1);
+      return  name.charAt(0).toUpperCase() + name.slice(1);
      }
      
           static sanitize  (name) {
